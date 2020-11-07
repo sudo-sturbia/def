36fd5b7f3d8ca0a02f2d447d8f8a514d66c02bc9
Zyad A. Ali <zyad.ali.me@gmail.com>
2020-11-07 14:49:10 +0200

    main: replace CONFIG_FILE and CONFIG_DIR with functions
    Previously, every time CONFIG_FILE or CONFIG_DIR were used, function
    full_path was called to generate an absolute path for each, as both were
    specified relative to $HOME. So, full_path was removed and both
    constants were replaced with functions that return an absolute path
    instead.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,6 @@ use command::Config;
 use ddir::Describer;
 use errors::Handle;
 
-static CONFIG_FILE: &str = ".config/ddir/config.json";
-static CONFIG_DIR: &str = ".config/ddir";
-
 const JSON_PRETTY: bool = true; // Use pretty JSON
 
 fn main() {
@@ -81,14 +78,14 @@ fn usage() {
     process::exit(1);
 }
 
-/// add_description creates a describer, either from CONFIG_FILE if it exists,
+/// add_description creates a describer, either from config_file if it exists,
 /// or empty otherwise. Maps the given description to config.path, and (re)writes
-/// the describer to CONFIG_FILE.
+/// the describer to config_file.
 fn add_description(config: &Config, description: &str) {
-    let mut describer = if Path::new(&full_path(CONFIG_FILE)).exists() {
+    let mut describer = if Path::new(&config_file()).exists() {
         get_describer()
     } else {
-        fs::create_dir_all(&full_path(CONFIG_DIR)).extract_or_exit("failed to create config");
+        fs::create_dir_all(&config_dir()).extract_or_exit("failed to create config");
         Describer::new()
     };
 
@@ -99,7 +96,7 @@ fn add_description(config: &Config, description: &str) {
     }
 
     fs::write(
-        &full_path(CONFIG_FILE),
+        &config_file(),
         describer
             .to_json(JSON_PRETTY)
             .extract_or_exit("failed to create config"),
@@ -107,7 +104,7 @@ fn add_description(config: &Config, description: &str) {
     .extract_or_exit("failed to write config to file")
 }
 
-/// print_description creates a describer using CONFIG_FILE, and prints
+/// print_description creates a describer using config_file, and prints
 /// a description of the path specified in config.path. If no description
 /// exists, an error message is printed.
 fn print_description(config: &Config) {
@@ -122,21 +119,27 @@ fn print_description(config: &Config) {
     )
 }
 
-/// get_describer loads JSON from CONFIG_FILE, creates a describer and
+/// get_describer loads JSON from config_file, creates a describer and
 /// returns it. Exits on error.
 fn get_describer() -> ddir::Describer {
     Describer::new_from_json(
-        &fs::read_to_string(&full_path(CONFIG_FILE)).extract_or_exit("failed to read config"),
+        &fs::read_to_string(&config_file()).extract_or_exit("failed to read config"),
     )
     .extract_or_exit("invalid JSON config")
 }
 
-/// full_path takes a path relative to $HOME and returns an absolute path.
-/// Exits on failure.
-fn full_path(relative: &str) -> String {
+/// config_file returns path to configuration file.
+fn config_file() -> String {
     format!(
-        "{}/{}",
+        "{}/.config/ddir/config.json",
         env::var("HOME").extract_or_exit("failed to get $HOME"),
-        relative
+    )
+}
+
+/// config_dir returns path to directory containing configuration file.
+fn config_dir() -> String {
+    format!(
+        "{}/.config/ddir",
+        env::var("HOME").extract_or_exit("failed to get $HOME"),
     )
 }
